8322c6b993fdccee9d1ad61b90e5aadac242b60c
chadfennell <libsys@gmail.com>
2018-08-30 11:05:59 -0500

    better arg descriptions

--- program.js
+++ program.js
@@ -3,9 +3,9 @@ const program = require('commander')
 program
   .version('0.1.0')
   .option('-i, --inputFile <filename>', 'Gzipped file to search')
-  .option('-r, --regexURL [URL]', 'A URL to a JSON array of regex patterns')
-  .option('-d, --batchDir <batch directory>', 'Directory to store matches (defaults to current)', 'matches')
-  .option('-b, --bucket [S3 Bucket]', 'AWS S3 bucket', false)
+  .option('-r, --regexURL [URL]', 'A URL to a JSON array of regex patterns (example: http://hub-client.lib.umn.edu/lookups/34)')
+  .option('-d, --batchDir <batch directory>', 'Directory to store matches', 'matches')
+  .option('-b, --bucket [S3 Bucket]', 'AWS S3 bucket (optional)', false)
   .parse(process.argv);
 
 module.exports.program = program;
