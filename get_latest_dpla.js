1c5233fb897d8aa582eabcca296b936217d7d3f6
Chad Fennell <libsys@gmail.com>
2019-03-18 11:06:29 -0500

    DPLA ships dumps in multiple files now, so download each of them

--- get_latest_dpla.js
+++ get_latest_dpla.js
@@ -11,13 +11,14 @@ var bucket = 'dpla-provider-export';
 
 function getLatestDpla() {
   var today = new Date();
-  prefix = `${today.getFullYear()}/${today.getMonth() + 1}/all`
+  var month = ('0' + (today.getMonth()+1)).slice(-2)
+  prefix = `${today.getFullYear()}/${month}/all.jsonl/`
   return S3.listObjects({Bucket: bucket, Prefix: prefix}, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else {
       let items = data.Contents
       if (items.length > 0 ) {
-        saveExport(items[items.length -1].Key)
+        items.map((item) => saveExport(item.Key))
       } else {
         console.log(`No New DPLA Dump Available For Download for ${prefix}`)
       }
@@ -26,13 +27,18 @@ function getLatestDpla() {
 }
 
 function saveExport(key) {
-  console.log(`Downloading Fresh DPLA Dump: ${key}`)
-  var params = {
-    Bucket: bucket,
-    Key: key
-   };
-   var jsonFile = require('fs').createWriteStream('./dpla.json.gz');
-   S3.getObject(params).createReadStream().pipe(jsonFile);
+  filename = key.split('/')[key.split('/').length -1]
+  if (filename.match(/^part/i)) {
+    console.log(`Downloading Fresh DPLA Dump: ${key}`)
+    var params = {
+      Bucket: bucket,
+      Key: key
+     };
+
+     fs.writeFile(`./dpla_data/${filename}`, '', (err) => { console.log(err) });
+     var jsonFile = require('fs').createWriteStream(`./dpla_data/${filename}`);
+     S3.getObject(params).createReadStream().pipe(jsonFile);
+  }
 }
 
 getLatestDpla();
