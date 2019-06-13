ac410084bc1edc18c61e5d7d1d0b89e92f656f3c
Chad Fennell <libsys@gmail.com>
2019-06-13 13:25:02 -0500

    idempotently perform init tasks on ingest instead of making people run an init script; make dpla ingest configurable

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir logs;
-touch logs/batches.log
-touch logs/matches.log
-touch logs/keys.log
-
-mkdir dpla_data;
--- run_dpla.js
+++ run_dpla.js
@@ -1,22 +1,48 @@
+const { program } = require('./program');
 const AWS = require('aws-sdk');
 const fs = require('fs');
 const { app } = require('./app');
-// const { localKeys } = require('./local_keys');
-const { s3Push } = require('./s3_push');
 const { wipeLocalData } = require('./wipe_data');
 
 const S3 = new AWS.S3({
   signatureVersion: 'v4'
 });
 
-var bucket = 'dpla-provider-export';
-var MATCH_BUCKET = 'ul-haystacker-umbra'
+// Config
+var logDir = './logs'
+var dplaBucket = 'dpla-provider-export';
+var dplaDataDir = './dpla_data';
+var batchDir = (program.batchDir ? program.batchDir : './matches');
+var regexURL = (program.regexURL ? program.regexURL : 'https://lib-metl-prd-01.oit.umn.edu/lookups/34.json');
+// Config
+
+init();
+
+// Create log files and directories
+function init() {
+
+  var dirs = [logDir, batchDir, dplaDataDir];
+  var logFiles = ['logs/batches.log', 'logs/matches.log', 'logs/keys.log'];
+  dirs.map(dir => { mkDir(dir); console.log(`mkdir ${dir}`) });
+  logFiles.map(path => { touch(path); console.log(`touch ${path}`) });
+
+  function touch(path) {
+    fs.closeSync(fs.openSync(path, 'w'));
+  }
+
+  function mkDir(dir) {
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+  }
+}
+
 
 function getLatestDplaAndMatch() {
   var today = new Date();
   var month = ('0' + (today.getMonth()+1)).slice(-2)
   prefix = `${today.getFullYear()}/${month}/all.jsonl/`
-  return S3.listObjects({Bucket: bucket, Prefix: prefix}, function(err, data) {
+  return S3.listObjects({Bucket: dplaBucket, Prefix: prefix}, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else {
       let items = data.Contents
@@ -34,11 +60,11 @@ function saveExport(key) {
   if (filename.match(/^part/i)) {
     console.log(`Downloading Fresh DPLA Dump: ${key}`)
     var params = {
-      Bucket: bucket,
+      Bucket: dplaBucket,
       Key: key
      };
 
-     fs.writeFile(`./dpla_data/${filename}`, '', (err) => { console.log(err) });
+     fs.writeFile(`${dplaDataDir}/${filename}`, '', (err) => { console.log(err) });
      var jsonFile = require('fs').createWriteStream(`./dpla_data/${filename}`);
      S3.getObject(params).createReadStream().pipe(jsonFile);
 
@@ -50,10 +76,10 @@ function saveExport(key) {
 
 function runMatcher(inputFile) {
   console.log(`Running Matcher On: ${inputFile}`);
-  app({ regexURL: 'https://lib-metl-prd-01.oit.umn.edu/lookups/34.json',
+  app({ regexURL: regexURL,
       inputFile: inputFile,
-      bucket: MATCH_BUCKET,
-      batchDir: 'matches'
+      bucket: false,
+      batchDir: batchDir
   }).subscribe(
     () => {},
     (err) => { console.log(err) },
@@ -63,40 +89,8 @@ function runMatcher(inputFile) {
   );
 }
 
-async function haystackBuketItems() {
-  let isTruncated = true;
-  let marker;
-  let items = []
-  while(isTruncated) {
-    let params = { Bucket: MATCH_BUCKET };
-    if (marker) params.Marker = marker;
-    try {
-      const response = await S3.listObjects(params).promise();
-      items = items.concat(response.Contents)
-      isTruncated = response.IsTruncated;
-      if (isTruncated) {
-        marker = response.Contents.slice(-1)[0].Key;
-      }
-  } catch(error) {
-      throw error;
-    }
-  }
-  return items;
-}
-
-async function s3delete(item) {
-  await S3.deleteObject({ Bucket: MATCH_BUCKET, Key: item.Key }, function(err, data) {
-    if (err) console.log(err, err.stack);  // error
-    else     console.log(`${item.Key} Deleted`);                 // deleted
-  });
-}
-
 async function run() {
   wipeLocalData();
-  let items =  await haystackBuketItems();
-  items.map( (item) => {
-    s3delete(item)
-  })
   await getLatestDplaAndMatch();
 }
 
