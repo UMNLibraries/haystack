0bd0dc345e4deb6accf43704caf016cdf3ca11b3
Chad Fennell <libsys@gmail.com>
2019-06-17 09:54:58 -0500

    batches becomes matches

--- program.js
+++ program.js
@@ -4,7 +4,7 @@ program
   .version('0.1.0')
   .option('-i, --inputFile <filename>', 'Gzipped file to search')
   .option('-r, --regexURL [URL]', 'A URL to a JSON array of regex patterns (example: https://lib-metl-prd-01.oit.umn.edu/lookups/34)')
-  .option('-d, --batchDir <batch directory>', 'Directory to store matches', 'matches')
+  .option('-d, --matchesDir <matches directory>', 'Directory to store matches', 'matches')
   .option('-b, --bucket [S3 Bucket]', 'AWS S3 bucket (optional)', false)
   .parse(process.argv);
 
